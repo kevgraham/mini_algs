a08b749775be0b42451c3005f14ca0e1746ef6f6
Kevin Graham <kevin.edward.graham@gmail.com>
2018-04-25 16:45:40 +0800

    fibonacci

---
+++ src/Fibonacci.java
@@ -0,0 +1,26 @@
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(2));
+        System.out.println(fibonacci(3));
+        System.out.println(fibonacci(4));
+        System.out.println(fibonacci(6));
+    }
+
+    public static int fibonacci(int n) {
+
+        // ensure proper usage
+        if (n <= 0) {
+            return 0;
+        }
+
+        // base case
+        else if (n == 1) {
+            return 1;
+        }
+        // recursion
+        else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
