16a75724fe297d4feb190f3090562ceb24a9f911
Kevin Graham <kevin.edward.graham@gmail.com>
2018-04-30 11:59:17 +0800

    palindromenumber leetcode

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,36 @@
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(123454321)); // true
+        System.out.println(isPalindrome(10));        // false
+        System.out.println(isPalindrome(121));       // true
+        System.out.println(isPalindrome(-121));      // false
+    }
+
+    public static boolean isPalindrome(int x) {
+
+        int temp = x;
+        int reverse = 0;
+
+        // reverse input
+        while (temp > 0) {
+
+            // get last digit
+            int last = temp % 10;
+
+            // append to beginning
+            reverse = (reverse * 10) + last;
+
+            // move to next digit
+            temp /= 10;
+        }
+
+        // check if reverse matches original
+        if (reverse == x) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
