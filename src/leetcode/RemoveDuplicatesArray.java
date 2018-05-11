bc83ce363167e8516540f2f3533987195b4fb9b1
Kevin Graham <kevin.edward.graham@gmail.com>
2018-05-11 11:27:50 +0800

    refactor

--- src/leetcode/RemoveDuplicatesArray.java
+++ src/leetcode/RemoveDuplicatesArray.java
@@ -1,3 +1,5 @@
+package leetcode;
+
 public class RemoveDuplicatesArray {
 
     public static void main(String[] args) {
--- src/misc/FizzBuzz.java
+++ src/misc/FizzBuzz.java
@@ -1,3 +1,5 @@
+package misc;
+
 public class FizzBuzz {
 
     public static void main(String[] args) {
