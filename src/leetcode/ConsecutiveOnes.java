6e2ba6046329ed11ca66ec210ae52d63eb20c295
Kevin Graham <kevin.edward.graham@gmail.com>
2018-06-29 11:22:00 -0400

    leetcode

---
+++ src/leetcode/ConsecutiveOnes.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+public class ConsecutiveOnes {
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+
+        int max = 0;
+        int cur = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                cur++;
+            }
+            else {
+                // set new max
+                if (cur > max) {
+                    max = cur;
+                }
+                // reset cur
+                cur = 0;
+            }
+        }
+
+
+        if (cur > max) {
+            max = cur;
+        }
+
+        return max;
+    }
+    
+}
